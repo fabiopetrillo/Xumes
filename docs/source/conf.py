5f23b8b9eae3d8f5e7d2a6d604516dd237d4d34b
mastainvin <vincent.mastain@outlook.fr>
2023-06-12 17:04:11 -0400

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Xumes'
+copyright = '2023, Vincent Mastain'
+author = 'Vincent Mastain'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0'
+version = '0.0.13'
 
 # -- General configuration
 
