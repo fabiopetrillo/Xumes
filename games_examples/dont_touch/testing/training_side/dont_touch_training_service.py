8c2d687ee6663d1ded71c72471139a0873962541
Amina Ferraoun <35732195+AminaFerraoun@users.noreply.github.com>
2023-07-03 16:37:35 +0200

    adding entities

--- games_examples/dont_touch/testing/training_side/dont_touch_training_service.py
+++ games_examples/dont_touch/testing/training_side/dont_touch_training_service.py
@@ -24,3 +24,13 @@ class   DontTouchTrainingService(StableBaselinesTrainer):
         self.score = 0
 
 
+    def convert_terminated(self) -> bool:
+        return self.game_state == "lose"
+
+    def convert_actions(self, raws_actions) -> List[str]:
+        direction = ["nothing", "left", "right"]
+        position = ["nothing", "up"]
+
+
+        self.actions = [direction[raws_actions[0]], position[raws_actions[1]], attack[raws_actions[2]]]
+        return self.actions
